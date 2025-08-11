8ce23cce7efb9ba297ed1a49a3900af0286deb34
ryusoh <zhuangliulz@gmail.com>
2025-08-10 17:16:07 -0700

    File name changes.

--- js/config.js
+++ js/config.js
@@ -64,8 +64,8 @@ const opacity = 0.7;
 
 export const TICKER_TO_LOGO_MAP = {
     'GEO': { src: '/img/logo/geo.png', scale: 1, rotation: 0, renderAsWhite: renderAsWhite, opacity: opacity },
-    'ANET': { src: '/img/logo/aristanw.png', scale: 0.7, rotation: false, renderAsWhite: renderAsWhite, opacity: opacity },
-    'GOOG': { src: '/img/logo/google.png', scale: 0.9, rotation: false, renderAsWhite: renderAsWhite, opacity: opacity },
+    'ANET': { src: '/img/logo/anet.png', scale: 0.7, rotation: false, renderAsWhite: renderAsWhite, opacity: opacity },
+    'GOOG': { src: '/img/logo/goog.png', scale: 0.9, rotation: false, renderAsWhite: renderAsWhite, opacity: opacity },
     'PDD': { src: '/img/logo/pdd.png', scale: 0.65, rotation: false, renderAsWhite: renderAsWhite, opacity: opacity },
     'OXY': { src: '/img/logo/oxy.png', scale: 1.3, rotation: false, renderAsWhite: renderAsWhite, opacity: opacity },
     'BRK-B': { src: '/img/logo/brk.png', scale: 2, rotation: 76, renderAsWhite: renderAsWhite, opacity: opacity },
